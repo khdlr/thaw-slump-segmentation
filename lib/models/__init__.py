a621216438f54dea4948e73709bdf9f1081b7029
Konrad Heidler <k.heidler@tum.de>
2022-11-02 09:44:50 +0100

    S2 Stuff

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -6,6 +6,7 @@ from .fpn import FPN
 from .pspnet import PSPNet
 from .deeplabv3 import DeepLabV3, DeepLabV3Plus
 from .pan import PAN
+from .plain_unet import PlainUNet
 import torch.nn as nn
 
 from . import encoders
@@ -16,35 +17,6 @@ from .__version__ import __version__
 
 from typing import Optional
 import torch
-
-
-def create_model(
-    arch: str,
-    encoder_name: str = "resnet34",
-    encoder_weights: Optional[str] = "imagenet",
-    in_channels: int = 3,
-    classes: int = 1,
-    **kwargs,
-) -> torch.nn.Module:
-    """Models wrapper. Allows to create any model just with parametes
-
-    """
-
-    archs = [Unet, UnetPlusPlus, MAnet, Linknet, FPN, PSPNet, DeepLabV3, DeepLabV3Plus, PAN]
-    archs_dict = {a.__name__.lower(): a for a in archs}
-    try:
-        model_class = archs_dict[arch.lower()]
-    except KeyError:
-        raise KeyError("Wrong architecture type `{}`. Avalibale options are: {}".format(
-            arch, list(archs_dict.keys()),
-        ))
-    return model_class(
-        encoder_name=encoder_name,
-        encoder_weights=encoder_weights,
-        in_channels=in_channels,
-        classes=classes,
-        **kwargs,
-    )
 
 
 def create_loss(
@@ -66,7 +38,7 @@ def create_loss(
     builtin_losses_dict = {l.__name__.lower(): l for l in builtin_losses}
     name = name.lower()
     if name in losses_dict:
-        return losses_dict[name](mode=losses.BINARY_MODE)
+        return losses_dict[name](mode=losses.BINARY_MODE, ignore_index=255)
     elif name in builtin_losses_dict:
         return builtin_losses_dict[name]()
     raise KeyError("Wrong loss type `{}`. Available options are: {}".format(
